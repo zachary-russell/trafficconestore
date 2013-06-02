8b03504c913bdfe632f07a2d864f6ecede60e411
tt sphinx <tam326@drexel.edu>
2013-06-02 00:46:06 -0400

    Fixed naming of menu location, applied styling

---
+++ categories-nav.php
@@ -0,0 +1,5 @@
+<div id="tertiary-nav-container">
+    <div class="wrap">
+        <h1>NO FUCKING WAY</h1>
+    </div>
+</div>
--- tertiary-nav.php
+++ tertiary-nav.php
@@ -1,10 +1,12 @@
-<div id="tertiary-nav-container"><div class="wrap">
+<div id="tertiary-nav-container">
+    <div class="wrap">
 <?php
 wp_nav_menu(array(
     'sort_column'       => 'menu_order',
     'container_id'      => 'tertiary-nav',
     'menu_class'        => 'menu tertiary-nav-menu',
-    'theme_location'    => 'tertiary_nav'
+    'theme_location'    => 'tertiary'
 ));
 ?>
-</div></div>
+    </div>
+</div>
