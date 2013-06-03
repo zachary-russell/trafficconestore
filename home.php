34e2d65f11380a614804f3d65fd627d251863693
tt thalia <tam326@cs.drexel.edu>
2013-06-02 21:05:17 -0400

    Moved homepage post layout to where it belongs, moved title to unter image

--- home.php
+++ home.php
@@ -27,10 +27,28 @@ function home_archive_post_class($classes) {
 remove_action('genesis_entry_header', 'genesis_do_post_title');
 remove_action('genesis_entry_header', 'genesis_post_info');
 remove_action('genesis_entry_footer', 'genesis_post_meta');
-// remove_action('genesis_entry_content', 'genesis_do_post_image');
 remove_action('genesis_entry_content', 'genesis_do_post_content');
+remove_action('genesis_entry_content', 'genesis_do_post_image');
+add_action('genesis_entry_footer', 'genesis_do_post_title');
 add_action('genesis_entry_footer', 'new_post_info');
-add_action('genesis_entry_header', 'genesis_do_post_title');
+add_action('genesis_entry_content', 'do_post_image_placeholder');
 
+function do_post_image_placeholder() {
+	if (!is_singular() && genesis_get_option('content_archive_thumbnail')) {
+		$img = genesis_get_image(array( 
+			'format'  => 'html',
+			'size'    => genesis_get_option('image_size'),
+			'context' => 'archive',
+			'attr'    => genesis_attr('entry-image', array('output' => 'array'))
+		));
+
+		if (empty($img)) {
+            $attrs = genesis_attr('entry-image', array('output' => 'array'));
+            $img = '<img src="' . get_stylesheet_directory_uri() . '/images/placeholder.jpg" class="' . $attrs['class'] . '" />';
+        }
+
+        printf('<a href="%s" title="%s">%s</a>', get_permalink(), the_title_attribute('echo=0'), $img);
+	}
+}
 
 genesis();
